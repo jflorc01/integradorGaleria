fd55e4807315381e14a350320beaf75ccbfc4f47
Juan Álvaro Flórez <juanal.flocar@educa.jcyl.es>
2024-12-17 17:16:41 +0100

    Script redimensionar imágenes
    Hecho script para redimensionar y redimensionadas todas las imágenes de la carpeta img.
    Las imágenes redimensionadas se encontrarán provisionalmente en la carpeta 'imgOptim'

---
+++ js/optImagenes.js
@@ -0,0 +1,35 @@
+const sharp = require('sharp');
+const fs = require('fs');
+
+// const inputImage = '../img/AlitasPollo.webp';
+const files = fs.readdirSync('../img/');
+const outputDir = '../imgOptim/';
+const sizes = [480, 768, 1080];
+const formats = ['webp'];
+
+if(!fs.existsSync(outputDir)){
+    fs.mkdirSync(outputDir, {recursive: true});
+}
+
+function resize(rutaCompleta, nombre){
+    sizes.forEach(size => {
+        formats.forEach(format => {
+            sharp(rutaCompleta).resize(size).toFormat(format, {
+                quality: 80,
+            })
+            .toFile(`${outputDir}${nombre}-${size}.${format}`, (err, info) => {
+                if(err){
+                    console.error("ERROR al generar la imagen ${size}px en formato ${format}:", err);
+                }else{
+                    console.log(`Generada imagen ${size}px en formato ${format}:`, info);
+                }
+            });
+        });
+    });
+}
+
+const promises = files.map(imagen => resize('../img/'+imagen, imagen));
+
+Promise.all(promises)
+  .then(() => console.log('Done'))
+  .catch(e => console.error(e));
